40922a409c59a2756ca55e52df8e57fc31180d72
zakariasf <60485480+zakariasf@users.noreply.github.com>
2020-11-12 21:46:34 +0000

    f

---
+++ template/Cryptocop.Software.API/Cryptocop.Software.API.Repositories/migrations/createTables.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE Users(
+	id serial PRIMARY KEY,
+	fullName character varying NOT NULL,
+	email character varying UNIQUE NOT NULL,
+	hashedPassword character varying NOT NULL
+);
+
+CREATE TABLE Addresses(
+	id serial PRIMARY KEY,
+	userId int not null,
+	streetName character varying NOT NULL,
+	houseNumber character varying Not NULL,
+	ZipCode character varying not null,
+	country character varying not null,
+	city character varying not null,
+	FOREIGN KEY(userId) REFERENCES Users(id)
+);
